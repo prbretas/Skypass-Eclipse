8faab08c42acd00dfaa7f6eefe3c01199089946a
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2023-04-03 11:14:05 -0300

    atualizacao

--- totvs/src/main/java/br/com/totvs/client/model/Client.java
+++ totvs/src/main/java/br/com/totvs/client/model/Client.java
@@ -4,7 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 import lombok.Builder;
